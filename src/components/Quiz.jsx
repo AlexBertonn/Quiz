de87a713628311b98766bb9ca57a5d8a3d283dbd
Alex Berton <alexberton47@gmail.com>
2025-01-08 19:09:12 -0300

    Update Quiz
    Creating a shuffler for answers, using a 'sort' method with a 'Math.random'.

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -5,7 +5,11 @@ export default function Quiz(){
     const [ userAnswers, setUserAnswers] = useState([]);
     const activeQuestionIndex = userAnswers.length;
 
-    
+    //ABAIXO
+    //Criando uma forma de embaralhar as respostas, pois a primeira sempre será a correta, com isso vou fazer com que seja embaralhada com as demais. 
+    //Claro que apenas ao usuário, por isso foi criado a array desta constante abaixo, para que não altere o array inicial.
+    const shuffledAnswers = [...QUESTIONS[activeQuestionIndex].answers];
+    shuffledAnswers.sort(() => Math.random() -0.5)
 
     function handleSelectAnswer (selectedAnswer){
         setUserAnswers((prevUserAnswer) => {
@@ -19,7 +23,7 @@ export default function Quiz(){
             <div id="questions">
                 <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
                 <ul id="answers">
-                    {QUESTIONS[activeQuestionIndex].answers.map(answer => 
+                    {shuffledAnswers.map(answer => 
                         <li key={answer} className="answer">
                             <button onClick={() => handleSelectAnswer(answer)}>{answer}</button>
                         </li>) }
