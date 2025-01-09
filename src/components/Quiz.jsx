b7a6d6fcd398da248a5eec8b4b4c3a2298e047d1
Alex Berton <104038811+AlexBertonn@users.noreply.github.com>
2025-01-09 16:43:36 +0000

    Adding a completed quiz screen.

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,24 +1,32 @@
 import { useState } from "react";
 import QUESTIONS from '../questions.js';
+import imgTrophy from '../assets/quiz-complete.png'
 
 export default function Quiz(){
     const [ userAnswers, setUserAnswers] = useState([]);
     const activeQuestionIndex = userAnswers.length;
+    const quizCompleted = activeQuestionIndex === QUESTIONS.length;
+    
+    function handleSelectAnswer (selectedAnswer){
+        setUserAnswers((prevUserAnswer) => {
+            return [...prevUserAnswer, selectedAnswer]
+        });
+    };
+    if(quizCompleted){
+        return( <div id="summary">
+            <img src={imgTrophy} alt="Trophy" />
+            <h2>Quiz Completed!</h2>
+        </div>
+    )}
 
     //ABAIXO
     //Criando uma forma de embaralhar as respostas, pois a primeira sempre será a correta, com isso vou fazer com que seja embaralhada com as demais. 
     //Claro que apenas ao usuário, por isso foi criado a array desta constante abaixo, para que não altere o array inicial.
     const shuffledAnswers = [...QUESTIONS[activeQuestionIndex].answers];
     shuffledAnswers.sort(() => Math.random() -0.5)
-
-    function handleSelectAnswer (selectedAnswer){
-        setUserAnswers((prevUserAnswer) => {
-            return [...prevUserAnswer, selectedAnswer]
-        });
-    };
-
+    
     return(
-      <>
+        <>
         <div id="quiz">
             <div id="questions">
                 <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
