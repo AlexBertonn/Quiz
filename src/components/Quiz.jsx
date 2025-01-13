ce8a6f47e6e31a7525a9924707e4fd194ad1efcc
Alex Berton <alexberton47@gmail.com>
2025-01-13 18:55:44 -0300

    Initial condition for correct answer css
    I didn't really understand what Max did, but later I learn more how he did this.
    I didn't make this alone, I just followed the class.
    Sorry...

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -4,15 +4,31 @@ import imgTrophy from '../assets/quiz-complete.png'
 import QuestionTimer from "./QuestionTimer.jsx";
 
 export default function Quiz(){
+    const [ answerState, setAnswerState ] = useState('');
     const [ userAnswers, setUserAnswers] = useState([]);
-    const activeQuestionIndex = userAnswers.length;
+
+    const activeQuestionIndex = answerState === '' ? userAnswers.length : userAnswers.length -1;
     const quizCompleted = activeQuestionIndex === QUESTIONS.length;
     
     const handleSelectAnswer = useCallback(function handleSelectAnswer (selectedAnswer){
+        setAnswerState('answered')
         setUserAnswers((prevUserAnswer) => {
             return [...prevUserAnswer, selectedAnswer]
         });
-    }, []);
+
+        setTimeout(() => {
+            if(selectedAnswer === QUESTIONS[activeQuestionIndex].answers[0]){
+                setAnswerState('correct')
+            } else{
+                setAnswerState('wrong')
+            }
+
+            setTimeout(() => {
+                setAnswerState('')
+            }, 2000);
+            
+        }, 1000)
+    }, [activeQuestionIndex]);
 
     const handleSkipAnswer = useCallback(() => handleSelectAnswer(null), [handleSelectAnswer]);
 
@@ -41,10 +57,24 @@ export default function Quiz(){
 
                 <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
                 <ul id="answers">
-                    {shuffledAnswers.map(answer => 
+                    {shuffledAnswers.map(answer => {
+                        const isSelected = userAnswers[userAnswers.length -1] === answer;
+                        let cssClass = '';
+                        if(answerState === 'answered' && isSelected){
+                            cssClass= 'selected'
+                        }
+
+                        if(answerState === 'correct' || answerState === 'wrong' && isSelected){
+                          cssClass = answerState;  
+                        }
+
+                        return(
                         <li key={answer} className="answer">
-                            <button onClick={() => handleSelectAnswer(answer)}>{answer}</button>
-                        </li>) }
+                            <button onClick={() => handleSelectAnswer(answer)} className={cssClass}>
+                                {answer}
+                            </button>
+                        </li>)}
+                    )}
                 </ul>
             </div>
         </div>
